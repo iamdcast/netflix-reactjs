8bc10be76e5e8c380a4740336573a6220df06f88
David Castillo <david.castillo.0326@gmail.com>
2023-02-25 15:46:05 -0800

    First Commit

---
+++ src/Request.js
@@ -0,0 +1,12 @@
+const key = 'bd5dfb80e02181f1bf25593dbf788782'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=true`,
+};
+
+export default requests;
